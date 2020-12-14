b53bb6e725fe1d88c617a1be8957388f08b32ad0
var787 <hegde.rakshan@gmail.com>
2020-12-14 01:11:44 -0600

    commit tree 56cf71e3bd0d0ced995689d26eb7a9724f2992f7

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+// 	let i = 0;
+// 	while (i < gifts.length) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow`);
+//         i++
+// 	}
+
+// 	return gifts;
+// }
+
+function writeCards(name, event) {
+	let thankYouMessage = [];
+	for (let i = 0; i < name.length; i++) {
+		thankYouMessage.push(
+			`Thank you, ${name[i]}, for the wonderful ${event} gift!`
+		);
+	}
+	return thankYouMessage;
+}
+
+function countDown(num) {
+	while (num > 0) {
+		console.log(num);
+		num -= 1;
+	}
+	console.log(num);
+}
